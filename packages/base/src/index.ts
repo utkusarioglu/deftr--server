6348b806b438302246c785624eef299327f546c6
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-11-19 22:11:27 +0300

    Feature: add very basic server

--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -1,11 +1,18 @@
-import Express from 'express';
+import Express, { Request, Response } from 'express';
+import cors from 'cors';
+import { ServerResponse } from '@newspaper/api';
 
 const app = Express();
+app.use(cors());
 
-app.use('/', (req, res) => {
-  res.send('hello');
+app.use('/:request', (req: Request, res: Response) => {
+  const response: ServerResponse = {
+    test: req.params.request,
+    response: req.params.request.slice(-2),
+  };
+  res.send(JSON.stringify(response));
 });
-
-app.listen(3000, () => {
-  console.log('App is listening');
+const HTTP_PORT = 4000;
+app.listen(HTTP_PORT, () => {
+  console.log(`App is listening on ${HTTP_PORT}`);
 });
