2bcadcb7153cc93381807fbeeebcd71581773d57
Utku Sarioglu <utkusarioglu@hotmail.com>
2020-11-21 20:39:03 +0300

    Feature: Implement auth basics
    - Passportjs is put to use with this commit. Right now the data
    store (postgres) is not set, hence real login, logout functionality is
    not there.
    -  tsc -w output in all scripts is now directed to dev/null as they
    were clearing the screen making it hard to track what was going on.
    - Base index.ts was refactored in accordance with the auth
    implementation.
    - A new package, for postgres was implemented
    - a few packages in base have been removed and moved to auth as they
    are where these packages are supposed to be.

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,3 +1,57 @@
-export function authString() {
-  return 'auth ';
+import passport from 'passport';
+import { Application } from 'express';
+import passportLocal from 'passport-local';
+import expressSession from 'express-session';
+
+const LocalStrategy = passportLocal.Strategy;
+
+export function enableSession(app: Application) {
+  const expressSessionInit = expressSession({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  });
+  app.use(expressSessionInit);
+}
+
+export function enableAuth(app: Application) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      if (username === 'utku' && password == '1') {
+        return done(null, { username, password });
+      }
+      return done(null, false);
+    })
+  );
+  passport.serializeUser((user, done) => {
+    console.log('serializer', user);
+    done(JSON.stringify(user));
+  });
+  passport.deserializeUser((userId: string, done) => done(JSON.parse(userId)));
+
+  app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+      console.log('auth', user);
+      if (err) {
+        console.log('auth err', err);
+        return next(err);
+      }
+
+      if (!user) {
+        return res.send({ message: 'fail' });
+      }
+      console.log('before req login');
+      req.logIn(user, (err) => {
+        console.log('login fun');
+        if (err) {
+          console.log('err\n', err);
+          return next(err);
+        }
+        return res.send({ message: 'login success' });
+      });
+    })(req, res, next);
+  });
 }
--- packages/base/src/index.ts
+++ packages/base/src/index.ts
@@ -1,25 +1,23 @@
 import Express, { Request, Response } from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import { ServerResponse } from '@newspaper/api';
 import { createUserRoles } from '@newspaper/server-mock-data';
-import { authString } from '@newspaper/server-auth';
+import { enableAuth, enableSession } from '@newspaper/server-auth';
 
 // const { HTTP_PORT } = process.env;
 const HTTP_PORT = 4000;
 
 const app = Express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+enableSession(app);
 
 createUserRoles();
-
-app.use('/:request', (req: Request, res: Response) => {
-  const response: ServerResponse = {
-    test: req.params.request,
-    response: authString() + req.params.request.slice(-2),
-  };
-  res.send(JSON.stringify(response));
-});
 
 app.listen(HTTP_PORT, () => {
   console.log(`App is listening on ${HTTP_PORT}`);
 });
+
+enableAuth(app);
